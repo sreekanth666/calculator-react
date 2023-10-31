552eed7dcd51cba93f8baf7a398a8b91002812c8
Sreekanth K <srekthk@gmail.com>
2023-10-31 23:39:41 +0530

    Parentheses added for calculations

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -4,14 +4,22 @@ import './calculator.css'
 function Calculator() {
     const [expression, setExpression] = useState("")
     const [result, setResult] = useState("")
+    const [tempExpression, setTempExpression] = useState("‎")
     const [isDarkMode, setIsDarkMode] = useState(false)
-    const handleInput = (value) => {
+    const handleInput = (value, action) => {
         setExpression(expression + value)
+        if (action == false) {
+            setTempExpression(tempExpression+value)
+        }
+        else {
+            setTempExpression("")
+        }
     }
 
     const clear = () => {
         setExpression("")
         setResult("")
+        setTempExpression("0")
     }
 
     const calculate = () => {
@@ -24,10 +32,11 @@ function Calculator() {
 
     const expressionSlice = () => {
         setExpression(expression.slice(0, -1))
+        setTempExpression(tempExpression.slice(0, -1))
     }
 
     // TEST
-    console.log(isDarkMode);
+    // console.log(isDarkMode);
     return (
         <div className='container-fluid d-flex justify-content-center align-items-center' style={{height:"100vh",backgroundColor:isDarkMode?"black":"white"}}>
             <div className="calculator rounded-5 p-3 container-fluid" style={{backgroundColor:isDarkMode?"#17181a":"#f7f8fb"}}>
@@ -40,10 +49,10 @@ function Calculator() {
                         }
                     </div>
                     <div className={isDarkMode?"result fs-1 fw-medium text-light":"result fs-1 fw-medium"}>
-                        = {
+                        {
                             result?
-                            result :
-                            "0"
+                            `=${result}` :
+                            `‎${tempExpression}`
                         }
                     </div>
                 </div>
@@ -51,31 +60,33 @@ function Calculator() {
                     <div className="row gap-2 mb-3">
                         <div className={isDarkMode?"btn col bg-secondary text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={clear}>C</div>
                         <div className={isDarkMode?"btn col bg-secondary text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={expressionSlice}><i class="fa-solid fa-delete-left"></i></div>
-                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("/")}>/</div>
-                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("*")}>*</div>
+                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("/",true)}>/</div>
+                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("*",true)}>*</div>
                     </div>
                     <div className="row gap-2 mb-3">
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("7")}>7</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("8")}>8</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("9")}>9</div>
-                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("-")}>-</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("7",false)}>7</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("8",false)}>8</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("9",false)}>9</div>
+                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("-",true)}>-</div>
                     </div>
                     <div className="row gap-2 mb-3">
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("4")}>4</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("5")}>5</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("6")}>6</div>
-                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("+")}>+</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("4",false)}>4</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("5",false)}>5</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("6",false)}>6</div>
+                        <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => handleInput("+",true)}>+</div>
                     </div>
                     <div className="row gap-2 mb-3">
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("1")}>1</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("2")}>2</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("3")}>3</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("1",false)}>1</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("2",false)}>2</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("3",false)}>3</div>
                         <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={calculate}>=</div>
                     </div>
                     <div className="row gap-2 mb-3">
                         <div className="btn col text-light d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4" style={{height:"55px",backgroundColor:"#23a056"}} onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode?<i class="fa-solid fa-sun"></i>:<i class="fa-solid fa-moon"></i>}</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("0")}>0</div>
-                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput(".")}>.</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("0",false)}>0</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput(".",false)}>.</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput("(",false)}>(</div>
+                        <div className={isDarkMode?"btn col bg-dark text-green d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4":"btn col bg-white d-flex justify-content-center align-items-center rounded-4 fw-semibold fs-4"} style={{height:"55px"}} onClick={() => handleInput(")",false)}>)</div>
                     </div>
                 </div>
             </div>
